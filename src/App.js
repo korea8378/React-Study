aa06baf44db558f487d356fdbbdfddcde071469a
korea8378 <korea8378@naver.com>
2019-08-06 22:40:00 +0900

    [MASTER][ADD] 자식 컴포넌트 -> 부모 컴포넌트로 data 전달

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import PhoneForm from "./components/PhoneForm";
 
-function App() {
-  return (
-    <div className="App">
-      <PhoneForm />
-    </div>
-  );
+class App extends Component {
+  id = 0;
+  state = {
+    information: []
+  };
+  handleCreate = data => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ ...data, id: this.id++ })
+    });
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
+      </div>
+    );
+  }
 }
 
 export default App;
