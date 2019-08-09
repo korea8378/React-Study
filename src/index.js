4a67c2a03e96c48d973595a0f7655c463a706c2b
korea8378 <korea8378@naver.com>
2019-08-09 19:45:11 +0900

    [MASTER][ADD] axios를 이용한 간단한 게시판

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Root from "./client/Root";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
 import * as serviceWorker from "./serviceWorker";
+import Promise from "promise-polyfill";
+import "./Animation.css";
 
-ReactDOM.render(<Root />, document.getElementById("root"));
-
+ReactDOM.render(<App />, document.getElementById("root"));
+if (!window.Promise) {
+  window.Promise = Promise;
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
